fabd629e03c454c00ed7ff50c057697e808b1b1e
Your Name <you@example.com>
2026-04-17 21:59:09 +0300

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,35 @@
-import {server, admin} from './server/server.js';
+import { createServer } from "./server/create-server.js";
+import routes from "./server/routes/api.routes.js";
+
+import initializeDb from "./db/create-db.js";
+import initializeAdminApp from "./admin/create-admin.js";
+import adminOptions from "./admin/options.js";
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    const { db } = await initializeDb();
 
-    server.listen(port, () => {
-      console.log(`Server available at http://localhost:${port}${admin.options.rootPath}`);
+    const { admin, router: adminRouter } = await initializeAdminApp({
+      options: await adminOptions(db),
+    });
+
+    const app = createServer();
+
+    // Routes
+    app.use(admin.options.rootPath, adminRouter);
+    app.use("/api", routes);
+
+    // Start
+    app.listen(port, () => {
+      console.log(
+        `Server running at http://localhost:${port}${admin.options.rootPath}`
+      );
     });
 
   } catch (error) {
-    console.error('Error starting the application:', error);
+    console.error("Error starting the application:", error);
   }
 };
 
